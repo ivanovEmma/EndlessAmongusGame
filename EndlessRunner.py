88f19c782ad0c3881c11e9a0525e0ed1aec99bb8
clarhenz <122268916+clarhenz@users.noreply.github.com>
2023-06-02 00:28:13 +0200

    Add files via upload

--- EndlessRunner.py
+++ EndlessRunner.py
@@ -17,11 +17,10 @@ def chrono():
     global s, run, score_var
     if run==True:
         fenetre.after(1000,chrono)
-        print(s)
         s=s+1
         score_var.set(f"Score: {s}")
 
-#Fonction upP: Lorsque la touche "Up" est appuyée, le personnage saute et retombe après 2 secondes
+#Fonction upP: Lorsque la touche "Up" est appuyée, le personnage saute et retombe après 1 seconde
 def upP(event):
     global X,Y,BASE_Y,InAir
     touche = event.keysym
@@ -29,7 +28,7 @@ def upP(event):
         Y = BASE_Y - DEP
         Canevas.coords(rond,0 ,80)
         InAir = True
-        Canevas.after(2000, down)
+        Canevas.after(1000, down)
         
 #Fonction down(): Lorsque le button "Up" est relachée, le personnage tombe par terre
 def down():
@@ -40,8 +39,10 @@ def down():
     InAir = False
 
 #Fonction obst(): controlle l'obstacle
+c = 0
 def obst():
-    global X,Y,run, OX, OY, speed
+    global X,Y,run, OX, OY, speed,c
+    
     if run == True:
         run = contacte()
         #print(contacte())
@@ -55,7 +56,6 @@ def obst():
             speed = speed * 1.0000001
 
         Canevas.coords(obstacle,OX, OY , OX + 20, OY + 20)
-
         if OX > -30:
         # mise à jour toutes les 50 ms
             run = contacte()
@@ -63,6 +63,14 @@ def obst():
 
 
         else :
+            #change la couleur de l'obstacle
+            c = c + 1
+            if(c%3 == 0):
+                Canevas.itemconfig(obstacle, fill='white')
+            elif(c%3 == 1):
+                Canevas.itemconfig(obstacle, fill='red')
+            elif(c%3 == 2):
+                Canevas.itemconfig(obstacle, fill='blue')
             OX = bOX
             run = contacte()
             fenetre.after(50,obst)
@@ -75,7 +83,7 @@ def obst():
 def contacte():
     global InAir
     if -10<OX<40 and InAir == False:
-        rond = Canevas.create_image(0,0,anchor=NW,image=death)
+        rond = Canevas.create_image(0,0,anchor=NW,image= death)
         print("kkv",InAir)
         return False
     else:
@@ -87,6 +95,7 @@ def jeu():
     run = True
     speed = 1.00
     obst()
+
 
 
 # Création de la fenêtre principale
@@ -101,15 +110,15 @@ BASE_Y = 150
 Y = BASE_Y
 bOX = X+320
 OX = bOX
-OY = BASE_Y
-Canevas = Canvas(fenetre,width=300,height=200,bg ='cyan')
+OY = BASE_Y - 20
+Canevas = Canvas(fenetre,width=300,height=200,bg ='black')
 # Création d'un objet graphique
 img = ImageTk.PhotoImage(Image.open("textures/walkingAnim/walk.png"))
-death = ImageTk.PhotoImage(Image.open("textures/skulle.png"))
+death = ImageTk.PhotoImage(Image.open("textures/dead.png"))
 rond = Canevas.create_image(0,110,anchor=NW,image=img)
 
 obstacle = Canevas.create_rectangle(5,5,TAILLE*2,TAILLE*2,fill='white')
-terre = Canevas.create_rectangle(0,170,1000,1000,fill='yellow')
+terre = Canevas.create_rectangle(0,150,1000,1000,fill='white')
 # La méthode bind() permet de lier un événement avec une fonction
 Canevas.focus_set()
 Canevas.bind('<KeyPress>',upP)
@@ -125,4 +134,3 @@ score_label.pack(side=RIGHT)
 jeu()
 chrono()
 fenetre.mainloop()
-
